dff4c01c833d6b06628b3403e902e6d27087a4df
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-27 14:32:41 +0530

    Binary tree

---
+++ binarytree/binarytree/Node.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class Node
+{
+    public Node LeftNode { get; set; }
+
+    public Node RightNode { get; set; }
+
+    public int Data { get; set; }
+}
---
+++ binarytree/binarytree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace binarytree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+
+            binaryTree.Add(1);
+            binaryTree.Add(2);
+            binaryTree.Add(7);
+            binaryTree.Add(3);
+            binaryTree.Add(0);
+
+
+            binaryTree.TraversePreorder(binaryTree.Root);
+        }
+    }
+}
